0794a5a7e87562ee93e8c367cf12aef45b5f2506
yao1yao <627223510@qq.com>
2018-09-29 11:58:56 +0800

    完善结束充电功能

--- src/store/modules/stateBox.js
+++ src/store/modules/stateBox.js
@@ -6,6 +6,13 @@ const state = {
         showTime: 1000,
         display: false
     },
+    modal: {
+        display: false,
+        title: '温馨提示',
+        content: '确认结束',
+        btnName: '确认',
+        type: 'default'
+    },
     loading: false,
 }
 
@@ -15,6 +22,9 @@ const state = {
 const mutations = {
     [STATUS_EVENT.POP_UP_TOAST] (state, status) {
       state.toast = status
+    },
+    [STATUS_EVENT.POP_UP_MODAL](state,status){
+      state.modal = status
     },
     [STATUS_EVENT.SENDREQUEST] (state, status) {
         state.loading = status
